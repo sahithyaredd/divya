8f515ad8c4247bab2c9f807d037c2872e665993c
sahithyaredd <155939745+sahithyaredd@users.noreply.github.com>
2024-03-18 11:18:50 +0530

    Add files via upload

---
+++ haha.java
@@ -0,0 +1,15 @@
+class Test
+{
+public static void main(String args[])
+{
+int a=10;
+int b=5;
+int c=5;
+int x,y,
+try
+x=a/(b-c);
+catch(ArithmeticException e)
+{
+System.out.println("Exception is division by zero");
+y=a/(b+c);
+System.out.println("the value of y is"+y);
